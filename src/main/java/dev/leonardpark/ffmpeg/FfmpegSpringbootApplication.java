57589276cf6068a333e7053c9b11110d53479fce
Leo Wu <leo.wu.kw@gmail.com>
2023-01-16 12:30:43 +0800

    init project

---
+++ src/main/java/dev/leonardpark/ffmpeg/FfmpegSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.leonardpark.ffmpeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmpegSpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FfmpegSpringbootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/leonardpark/ffmpeg/FfmpegSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonardpark.ffmpeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfmpegSpringbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
